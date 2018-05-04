0efb2fb441687ee4ab8b4bd068100aaa78d80969
sweeneyyy <sweeneyarnett@gmail.com>
2018-05-04 13:27:07 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Summary
+A boilerplate for MERN stack!
+
+# Technologies Used
+* Mongodb
+* Express
+* React
+* Node.js
+
+# Backend Routes
+METHOD | URL | Purpose
+--- | --- | ---
+POST | /auth/signup | Adds new user to user database
+POST | /auth/login | Authenticates login details
+POST | /auth/me/from/token | Checks if token is present on browser refresh
+
+# Next Steps
+* Up to you!
---
+++ client/src/layout/Nav.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Logout from '../auth/Logout.js';
+import { Link } from 'react-router-dom';
+
+class Nav extends Component {
+  render(){
+    let links = <span />;
+    if(this.props.user){
+      links = (
+        <span>
+          <Link to="/profile">Profile</Link>
+          <Logout updateUser={this.props.updateUser} />
+        </span>);
+    }
+    else {
+      links = (
+        <span>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </span>);
+    }
+
+    return(
+        <div>
+          <nav className="nav">
+            <a href="/">Home</a>
+            {links}
+          </nav>
+        </div>
+      );
+  }
+}
+
+export default Nav;
