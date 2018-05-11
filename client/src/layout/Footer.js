8d14902300f9a113fad322062afad651cc189a75
sweeneyyy <sweeneyarnett@gmail.com>
2018-05-10 18:50:25 -0700

    starting on buy

--- client/src/layout/Footer.js
+++ client/src/layout/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 function Footer () {
   return(
     <div className="footer">
-        <span className="footer-text">&copy; {new Date().getFullYear()}</span>
+        <span className="footer-text">&copy; Sweeney Arnett {new Date().getFullYear()}</span>
     </div>
   );
 }
--- client/src/realestate/Buy.js
+++ client/src/realestate/Buy.js
@@ -4,7 +4,9 @@ class Buy extends Component {
   render(){
     return (
       <div>
-        <h1>BUY</h1>
+        <h1>Buy with Sharon & Caron</h1>
+          <p>Lorem ipsum dolor amet plaid austin unicorn lumbersexual pug. You probably haven't heard of them tilde single-origin coffee fixie, vape humblebrag artisan raclette quinoa blue bottle edison bulb DIY vexillologist. Deep v mlkshk scenester woke bicycle rights four dollar toast palo santo cliche XOXO salvia kogi chartreuse. Waistcoat umami 8-bit tumblr. Try-hard blog hell of, asymmetrical seitan typewriter taxidermy lumbersexual letterpress crucifix blue bottle four loko.</p>
+          <img src="https://picsum.photos/g/1300/300/?random" />
       </div>
       );
   }
