731a6550ca09edb87574d6642c1103b7622c216c
sweeneyyy <sweeneyarnett@gmail.com>
2018-06-07 17:05:10 -0700

    list summary comp

---
+++ client/src/components/ListingSummary.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function ListingSummary(props) {
+  return (
+    <div>
+      <h1>List Price: </h1>
+      <p>Address: </p>
+    </div>
+  );
+}
+
+export default ListingSummary;
