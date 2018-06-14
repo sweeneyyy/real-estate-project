3f78f030f33121a02b6ba0b2a266456947f87a7c
sweeneyyy <sweeneyarnett@gmail.com>
2018-06-13 18:17:39 -0700

    agent bio and image

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -70,12 +70,12 @@ class Home extends Component {
           </header>
           <section className='home'>
             <h2>Sharon Arnett & Caron McGinley</h2>
-            <img src={agentphoto} alt='Agent Photo' className='agent-photo' />
             <p className='agent-bio'>Whether you are looking at buying, selling or making a difficult decision when facing a financial crisis with a piece of real estate, we can help.<br />
             <br />
             We have advised our clients over the years with all stages of decision-making in the acquisition and disposition of their real estate assets. There's no substitute for experience and we are experts in problem solving in this challenging market.<br /> 
             <br />
             The majority of our business is repeat and referral - references are no problem - just ask!</p>
+            <img src={agentphoto} alt='Agent Photo' className='agent-photo' />
           </section>
           <section className='home'>
             <h1>Featured Listings</h1>
